5159c52d9e99c75efc447160d7f2d4dbe8a4347a
Colin McNamara <CEG118@Robyns-Macintosh.local>
2016-10-06 16:14:24 -0400

    Initial Commit
    let’s try this commit again…

---
+++ footer.php
@@ -0,0 +1,27 @@
+<!-- footer.php -->
+<style>
+	.footer {
+		background-color: #1EA896;
+		color: #fff;
+		height: 200px;
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+		padding: 4px;
+	}
+</style>
+<div class="footer">
+	<div class="container">
+		<div class="col-md-3 col-xs-5">
+			Colin McNamara<br>
+			123 Main Street<br>
+			Pleasantville, USA 55555
+		</div>
+		<div class="md-9 col-xs-7">
+			<p><strong>About Me</strong></p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus expedita, recusandae! Sed asperiores, voluptatem neque nemo necessitatibus perspiciatis ad deserunt, ullam vero, adipisci soluta, mollitia. Blanditiis, et odit explicabo accusantium! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab illo ad soluta quibusdam placeat officia possimus magnam nemo perferendis repudiandae! Beatae alias, est voluptatibus blanditiis rerum eos et fugit placeat?</p>
+			
+
+		</div>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Colin's Repo Index</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+	<?php include_once("header.php") ?>
+	
+	<div class="page-header text-center">
+		<h1>Colin's Repo Index</h1>
+	</div>	
+	
+	
+
+	<?php include_once("footer.php") ?>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
