deade2ca1e5d7c6d9ccfcb072bc79d911eefbdf3
Colin McNamara <coolinmc6@gmail.com>
2016-10-11 22:59:58 -0400

    Finished video 4
    Remember to create a database to finish up

--- Udemy-Chat-php-ajax/db.php
+++ Udemy-Chat-php-ajax/db.php
@@ -1,1 +1,17 @@
 <!-- db.php -->
+<?php 
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	$db_name = "chat_app";
+
+
+	$con = new mysqli($host, $user, $pass, $db_name);
+
+
+
+
+
+
+
+ ?>
--- Udemy-Chat-php-ajax/index.php
+++ Udemy-Chat-php-ajax/index.php
@@ -1,4 +1,7 @@
 <!-- index.php -->
+<?php 
+	include 'db.php';
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,17 +12,24 @@
 <body>
 	<div id="container">
 		<div id="chat_box">
+		<?php 
+			$query = "SELECT * FROM chat ORDER BY id DESC";
+			$run = $con->query($query);
+
+			while($row = $run->fetch_array()) :
+		 ?>
 			<div id="chat_data">
-				<span style="color: blue;">Colin:</span>
-				<span style="color: black;">How are you? (static message)</span>
-				<span style="float: right;">12:30pm</span>
+				<span style="color: blue;"><?php echo $row['name'] ?></span>
+				<span style="color: black;"><?php echo $row['msg'] ?></span>
+				<span style="float: right;"><?php echo $row['date'] ?></span>
 			</div>
-			<form action="index.php" method="post">
-				<input type="text" name="name" placeholder="enter name"/>
-				<textarea name="enter_message" placeholder="enter message"></textarea>
-				<input type="submit" name="submit" value="Send it"/>
-			</form>
+		<?php endwhile; ?>
 		</div>
+		<form action="index.php" method="post">
+			<input type="text" name="name" placeholder="enter name"/>
+			<textarea name="enter_message" placeholder="enter message"></textarea>
+			<input type="submit" name="submit" value="Send it"/>
+		</form>
 	</div>
 	
 </body>
