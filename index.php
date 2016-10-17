e714ae580ed4e622d9a78c0607108f1c947cd8de
Colin McNamara <CEG118@Robyns-Macintosh.local>
2016-10-17 17:33:39 -0400

    Learning SASS
    Let’s see if this still works at home…

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8">
 	<title>Colin's Repo Index</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="shortcut icon" href="favicon.ico">
 </head>
 <body>
 	<?php include_once("header.php") ?>
@@ -13,6 +14,11 @@
 	<div class="page-header text-center">
 		<h1>Colin's Repo Index</h1>
 	</div>
+	<ul>
+		<li><a href="sass-test1.php">Sass Test #1</a></li>
+		<li><a href="PHP-and-MySQL/index.php">PHP and MySQL for Dynamic Websites</a></li>
+		<li><a href="Udemy-Chat-php-ajax/index.php">Udemy Chat App</a></li>
+	</ul>
 		
 	
 	
