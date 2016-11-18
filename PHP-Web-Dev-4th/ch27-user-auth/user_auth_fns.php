57373a1e08949b203fcdf469c73db561f282d582
Colin McNamara <coolinmc6@gmail.com>
2016-11-17 22:51:51 -0500

    touch-ups

--- PHP-Web-Dev-4th/ch27-user-auth/user_auth_fns.php
+++ PHP-Web-Dev-4th/ch27-user-auth/user_auth_fns.php
@@ -78,12 +78,90 @@
 
     $result = $conn->query("update user set passwd = sha1('".$new_password."')
       where username = '".$username."'");
-    
+
     if (!$result) {
       throw new Exception('Password could not be changed.');
     } else {
       return true; // changed successfully
     }
   }  
+
+  function reset_password($username) {
+  // set password for username to a random value
+  // return the new password or false on failure
+  // get a random dictionary word b/w 6 and 13 chars in length
+  $new_password = get_random_word(6, 13);
+  if($new_password == false) {
+  throw new Exception('Could not generate new password.');
+  }
+  // add a number between 0 and 999 to it
+  // to make it a slightly better password
+  $rand_number = rand(0, 999);
+  $new_password .= $rand_number;
+  // set user's password to this in database or return false
+  $conn = db_connect();
+  $result = $conn->query("update user
+  set passwd = sha1('".$new_password."')
+  where username = '".$username."'");
+  if (!$result) {
+  throw new Exception('Could not change password.'); // not changed
+  } else {
+  return $new_password; // changed successfully
+  }
+  }
+
+
+  function get_random_word($min_length, $max_length) {
+    // grab a random word from dictionary between the two lengths
+    // and return it
+    // generate a random word
+    $word = '';
+    // remember to change this path to suit your system
+    $dictionary = '/usr/share/dict/words'; // the ispell dictionary
+    $fp = @fopen($dictionary, 'r');
+    if(!$fp) {
+      return false;
+    }
+
+    $size = filesize($dictionary);
+    // go to a random location in dictionary
+    $rand_location = rand(0, $size);
+    fseek($fp, $rand_location);
+    // get the next whole word of the right length in the file
+    while ((strlen($word) < $min_length) || (strlen($word)>$max_length) ||
+    (strstr($word, "'"))) {
+      if (feof($fp)) {
+        fseek($fp, 0); // if at end, go to start
+      }
+      $word = fgets($fp, 80); // skip first word as it could be partial
+      $word = fgets($fp, 80); // the potential password
+    }
+    
+    $word = trim($word); // trim the trailing \n from fgets
+    return $word;
+  }
+
+  function notify_password($username, $password) {
+    // notify the user that their password has been changed
+    $conn = db_connect();
+    $result = $conn->query("select email from user where username='".$username."'");
+    if (!$result) {
+      throw new Exception('Could not find email address.');
+    } else if ($result->num_rows == 0) {
+      throw new Exception('Could not find email address.');
+      // username not in db
+    } else {
+      $row = $result->fetch_object();
+      $email = $row->email;
+      $from = "From: support@phpbookmark \r\n";
+      $mesg = "Your PHPBookmark password has been changed to ".$password."\r\n"
+      ."Please change it next time you log in.\r\n";
+      if (mail($email, 'PHPBookmark login information', $mesg, $from)) {
+        return true;
+      } else {
+        throw new Exception('Could not send email.');
+      }
+    }
+  }
 
 ?>
--- index.php
+++ index.php
@@ -11,38 +11,40 @@
 <body>
 	<?php include_once("header.php") ?>
 	<!-- <a href=""></a> -->
-	
-	<div class="page-header text-center">
-		<h1>Colin's Repo Index</h1>
-	</div>
-	<ul>
-		<li><a href="sass-test1.php">Sass Test #1</a></li>
-		<li><a href="PHP-and-MySQL/index.php">PHP and MySQL for Dynamic Websites</a></li>
-		<li><a href="Udemy-Chat-php-ajax/index.php">Udemy Chat App</a></li>
-		<li>DoubleKnot</li>
-		<ul>
-			<li>Darkened color background on an image</li>
-			<li>Here is the original site: <a href="http://www.doubleknotphilly.com/">DoubleKnot</a>.</li>
-			<li>They do an overlay div with some opacity (0.9) and make it black.  I did it another way but should play with both.</li>
-		</ul>
-		<li><a href="positioning1.php">Positioning</a></li>
-		<li><a href="parallax.php">Parallax</a></li>
-		<li><a href="stretchy-navigation/add.html">Stretchy Navigation: Add</a></li>
-		<li><a href="stretchy-navigation/edit.html">Stretchy Navigation: Edit</a></li>
-		<li><a href="stretchy-navigation/index.html">Stretchy Navigation: Index</a></li>
-
-	</ul>
-	<hr>
-	<div class="content">
-		<h1>Random Items</h1>
-		<button onClick="showDiv()">Show the Div</button>
-		<div id="confirm-div"></div>
-		<a href="jump.php" onClick="return confirm('Are you sure?')">Reload page if Yes</a>
-	</div>
+	<div id="main">
 		
 	
-	
+		<div class="page-header text-center">
+			<h1>Colin's Repo Index</h1>
+		</div>
+		<ul>
+			<li><a href="sass-test1.php">Sass Test #1</a></li>
+			<li><a href="PHP-and-MySQL/index.php">PHP and MySQL for Dynamic Websites</a></li>
+			<li><a href="Udemy-Chat-php-ajax/index.php">Udemy Chat App</a></li>
+			<li>DoubleKnot</li>
+			<ul>
+				<li>Darkened color background on an image</li>
+				<li>Here is the original site: <a href="http://www.doubleknotphilly.com/">DoubleKnot</a>.</li>
+				<li>They do an overlay div with some opacity (0.9) and make it black.  I did it another way but should play with both.</li>
+			</ul>
+			<li><a href="positioning1.php">Positioning</a></li>
+			<li><a href="parallax.php">Parallax</a></li>
+			<li><a href="stretchy-navigation/add.html">Stretchy Navigation: Add</a></li>
+			<li><a href="stretchy-navigation/edit.html">Stretchy Navigation: Edit</a></li>
+			<li><a href="stretchy-navigation/index.html">Stretchy Navigation: Index</a></li>
 
+		</ul>
+		<hr>
+		<div class="content">
+			<h1>Random Items</h1>
+			<button onClick="showDiv()">Show the Div</button>
+			<div id="confirm-div"></div>
+			<a href="jump.php" onClick="return confirm('Are you sure?')">Reload page if Yes</a>
+		</div>
+		
+		
+	
+	</div>
 	<?php include_once("footer.php") ?>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
