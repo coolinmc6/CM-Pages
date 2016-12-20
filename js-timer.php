adef3bc8abbc2771681e50d18a92c00c851c26dd
Colin McNamara <CEG118@Robyns-Macintosh.local>
2016-12-20 17:05:02 -0500

    Add object

--- js-timer.php
+++ js-timer.php
@@ -26,6 +26,10 @@
 			height: 40px;
 			font-size: 30px;
 		}
+		#stuff {
+			text-align: center;
+		}
+		
 	</style>
 	<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
 </head>
@@ -36,12 +40,21 @@
 			
 		</div>
 		<button class="restart">Restart</button>
+
 	</div>
+	<table id='stuff'>
+		<tr>
+			<th>Date</th>
+			<th>Time</th>
+			<th>Activity</th>
+		</tr>
+	</table>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<script>
 		var start = 0;
 		var seconds = 0;
 		var minutes = 0;
+		var objects = [];
 
 		
 		setInterval(function(){
@@ -67,8 +80,30 @@
 		}, 1000)
 		
 		$('.restart').on('click',function(){
+			var e = new Date();
+			var object = { date: e, time: Math.floor(start/60000,4) + ' mins' }
+			objects.push(object)
 			start = 0;
+			showItems(object);
 		})
+
+		function showItems(object){
+			var tr = document.createElement('tr');
+			var th1 = document.createElement('td');
+			th1.innerHTML = object['date'];
+			var th2 = document.createElement('td');
+			th2.innerHTML = object['time'];
+			var th3 = document.createElement('td');
+			th3.innerHTML = 'Stuff';
+			var stuff = document.getElementById('stuff')
+			tr.appendChild(th1)
+			tr.appendChild(th2)
+			tr.appendChild(th3)
+			stuff.appendChild(tr)
+			
+			
+			
+		}
 		
 	</script>
 	
