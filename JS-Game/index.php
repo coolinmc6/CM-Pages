83c477a3554d5a336f2acdbb332f69a8c84a6286
Colin McNamara <coolinmc6@gmail.com>
2017-06-20 10:20:52 -0400

    JS breakout

---
+++ JS-Game/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Breakout Game</title>
+	<link rel="stylesheet" href="../css/breakout.css">
+</head>
+<body>
+	<br>
+	<br>
+	<br>
+	<canvas id="mycanvas" width="480px" height="320px"></canvas>
+
+	<script src="script.js"></script>
+</body>
+</html>
